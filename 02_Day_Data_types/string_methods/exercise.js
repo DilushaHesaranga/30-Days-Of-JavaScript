1c5808dd0dd8a2bcdec2a24044f2cf7119355b07
DilushahHesaranga <dilushahesaranga@gmail.com>
2023-09-20 20:43:57 +0530

    Did the day work

--- 02_Day_Data_types/string_methods/exercise.js
+++ 02_Day_Data_types/string_methods/exercise.js
@@ -1,12 +1,22 @@
+let string = 'Dilusha Hesaranga';
 //Ends With
+console.log(string.endsWith('Hesaranga'));
 //Includes
-//last index
+console.log(string.includes('Jamuni'));
+//index
+console.log(string.indexOf("Dilusha"));
 //length
+console.log(string.length);
 //match
 //search
 //repeat
+console.log(string.repeat(10));
 //replace
+console.log(string.replace('Dilusha','Jamuni Dilusha'));
 //split
 //starts with
+console.log(string.startsWith('Dilusha'));
 //lowercase
+console.log(string.toLowerCase());
 //uppercase
+console.log(string.toUpperCase());
